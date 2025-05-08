10528e5939b5611858f6f63f553ba858fb5bec01
ابُْنًْ الًْشًْايًْبُْ 🇮🇶 <106197547+kmdkmv313@users.noreply.github.com>
2025-05-08 14:29:39 +0300

    Delete modules directory

--- modules/report_generator.py
+++
@@ -1,6 +0,0 @@
-def generate_html_report(results, username):
-    with open(f"{username}_report.html", "w") as f:
-        f.write("<h1>FalconTrace OSINT Report</h1>")
-        for site, found in results:
-            status = "✅ Found" if found else "❌ Not Found"
-            f.write(f"<p>{site} : {status}</p>")
