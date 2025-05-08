9cb9cdfb014bc73e97faf55634059c77b1e46ea9
ابُْنًْ الًْشًْايًْبُْ 🇮🇶 <106197547+kmdkmv313@users.noreply.github.com>
2025-05-08 14:29:29 +0300

    Delete core.py

--- core.py
+++
@@ -1,9 +0,0 @@
-from modules.checker import run_checks
-from modules.report_generator import generate_html_report
-
-with open("modules/sites_list.txt") as f:
-    sites = [line.strip() for line in f]
-
-username = input("Enter username: ")
-results = run_checks(username, sites)
-generate_html_report(results, username)
